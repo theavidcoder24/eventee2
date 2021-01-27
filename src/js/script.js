f8082f424e84ca254babeb404dfa41fe097a913f
Mallorie <malloriecini@outlook.com>
2021-01-27 22:46:06 +1000

    commit working

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+
+console.log("Hello");
+
+
+/* == Navigation == */
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+});
+
+/* == Modal == */
+$(document).ready(function () {
+  $('.modal').modal();
+  // closeOnClick: true;
+});
