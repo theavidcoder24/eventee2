f6b7f87e4f07aca19ebe241e366ad00041963057
theavidcoder24 <malloriecini@outlook.com>
2021-03-02 14:14:20 +1000

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -61,7 +61,7 @@ function errormessage(message) {
   }, 7000)
 }
 
-/* Display Sections */
+/* == Display Sections == */
 function hideHome() {
   document.getElementById("home").style.display = "none";
 }
@@ -204,6 +204,193 @@ function displayFAQ() {
 function hideFAQ() {
   document.getElementById("faq").style.display = "none";
 }
+
+
+/* == Fetch Responses == */
+/* - Login - */
+function postLoginFetch() {
+  loadPage();
+  var log_email = document.getElementById("log_email");
+  var log_pass = document.getElementById("log_pass");
+  var login_details = new FormData();
+  login_details.append('log_email', log_email.value);
+  login_details.append('log_pass', log_pass.value);
+  login_details.append('action', 'login');
+  // each form element goes into the login_details object ^
+  fetch('api/ws.php?action=login', {
+    method: 'POST',
+    body: login_details,
+    credentials: 'include',
+  })
+    .then(function (response) {
+      // Force error into console
+      // response.text().then(function (text) {
+      //   console.log(text);
+      // });
+      // HTTP Response Codes
+      if (response.status === 202) {
+        localStorage.setItem('login', "true");
+        successmessage('Yay Successfully Logged in!');
+        var log_email = document.getElementById("log_email");
+        var login_details = new FormData();
+        login_details.append('log_email', log_email.value);
+        fetch('api/ws.php?action=is_logged_in', {
+          method: 'GET',
+          body: login_details,
+          credentials: 'include',
+        })
+        console.log('Login Successful');
+        // Display Events Container
+        var eventContainer = document.querySelector("#eventContainer");
+        eventContainer.style.display = "block";
+        var createMenuItem = document.querySelector("#createMenuItem");
+        createMenuItem.style.display = "block";
+        var logoutMenuItem = document.querySelector("#logoutMenuItem");
+        logoutMenuItem.style.display = "block";
+        // var createEvent = document.getElementById("#createEvent");
+        // createEvent.style.display = "block";
+        // localStorage.getItem('Login Email', log_email);
+        // localStorage.getItem('Login Password', log_pass);
+      }
+      if (response.status === 401) {
+        console.log('Not permitted');
+        errormessage("Error: Not implemented");
+        // localStorage.setItem('login', "false");
+        fetch('api/ws.php?action=logout', {
+          method: 'GET'
+        })
+      }
+      if (response.status === 501) {
+        console.log('Not implemented');
+        errormessage("Error: Not implemented");
+        // localStorage.setItem('login', "false");
+        fetch('api/ws.php?action=logout', {
+          method: 'GET'
+        })
+        var eventContainer = document.querySelector("#eventContainer");
+        eventContainer.style.display = "none";
+        var createMenuItem = document.getElementById("#createMenuItem");
+        createMenuItem.style.display = "none";
+        var logoutMenuItem = document.querySelector("#logoutMenuItem");
+        logoutMenuItem.style.display = "none";
+        // var createEvent = document.querySelector("#createEvent");
+        // createEvent.style.display = "none";
+      }
+    })
+  // .catch(function (err) {
+  //   console.log("Connection unavailable");
+  //   console.log(err);
+  // });
+}
+
+
+/* - is logged in Fetch - */
+function isLogged() {
+  fetch('api/ws.php?action=is_logged_in', {
+    method: 'GET',
+    body: login_details,
+    credentials: 'include',
+  })
+    // HTTP Response Codes
+    .then(function (response) {
+      response.text().then(function (text) {
+        console.log(text);
+      });
+      if (response.status === 202) {
+        localStorage.setItem('login', "true");
+        console.log('Logged In!!');
+        loadPage();
+        // Display Events Container
+        var eventContainer = document.querySelector("#eventContainer");
+        eventContainer.style.display = "block";
+        // Create Events Menu Item
+        var createMenuItem = document.querySelector("#createMenuItem");
+        createMenuItem.style.display = "block";
+        // Logout Menu Item
+        var logoutMenuItem = document.querySelector("#logoutMenuItem");
+        logoutMenuItem.style.display = "block";
+        // Create Events modal
+        var createEvent = document.querySelector("#createEvent");
+        createEvent.style.display = "block";
+      }
+      if (response.status === 401) {
+        loadPage();
+        console.log('Not logged in failed');
+        errormessage("Error: Not implemented");
+        localStorage.setItem('login', "false");
+        // Display Events Container
+        var eventContainer = document.querySelector("#eventContainer");
+        eventContainer.style.display = "none";
+        // Create Events Menu Item
+        var createMenuItem = document.querySelector("#createMenuItem");
+        createMenuItem.style.display = "none";
+        // Logout Menu Item
+        var logoutMenuItem = document.querySelector("#logoutMenuItem");
+        logoutMenuItem.style.display = "none";
+        // Create Events modal
+        var createEvent = document.querySelector("#createEvent");
+        createEvent.style.display = "none";
+        return;
+      }
+      if (response.status === 429) {
+        console.log('Rate limit exceeded');
+      }
+    })
+  // .catch(function (err) {
+  //   console.log("Connection unavailable");
+  //   console.log(err);
+  // });
+}
+
+/* - User Logout - */
+function userLogout() {
+  fetch('api/ws.php?action=logout', {
+    method: 'GET'
+  })
+    .then(function (response) {
+      // Force error into console
+      response.text().then(function (text) {
+        console.log(text);
+      });
+      // HTTP Response Codes
+      if (response.status === 202) {
+        console.log("Logout Success");
+        successmessage("Success, You're Logged Out");
+        // Display Events Container
+        var eventContainer = document.querySelector("#eventContainer");
+        eventContainer.style.display = "none";
+        // Create Events Menu Item
+        var createEvent = document.querySelector("#createEvent");
+        createEvent.style.display = "none";
+        // Logout Menu Item
+        var logoutMenuItem = document.querySelector("#logoutMenuItem");
+        logoutMenuItem.style.display = "none";
+        // localStorage.removeItem('Login Email', reg_email);
+        // localStorage.removeItem('Login Password', reg_pass);
+        localStorage.setItem('login', "false");
+      }
+      // if (response.status === 401) {
+      //   console.log("Not permitted");
+      //   errormessage("Internal Server - Not Permitted");
+      //   return;
+      // }
+      // if (response.status === 501) {
+      //   console.log("Logout Failed");
+      //   return;
+      // }
+      if (response.status === 429) {
+        console.log('Rate limit exceeded');
+      }
+      else {
+        errormessage("Error - Internal Server error not logged out");
+      }
+    })
+  // .catch(function (err) {
+  //   console.log("Connection unavailable");
+  //   console.log(err);
+  // });
+}
+
 
 /* if logged in 201 display event created if logged in not 201 or 202 display message must be logged in */
 
