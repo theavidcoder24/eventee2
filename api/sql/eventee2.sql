95c6c662007b797500771b85c50328b3ee95f351
Mallorie <malloriecini@outlook.com>
2021-03-04 21:52:10 +1000

    Create eventee2.sql

---
+++ api/sql/eventee2.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 04, 2021 at 04:13 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eventee2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `changelog`
+--
+
+DROP TABLE IF EXISTS `changelog`;
+CREATE TABLE IF NOT EXISTS `changelog` (
+  `date` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `browser` char(200) NOT NULL,
+  `ip` char(15) NOT NULL,
+  `action_type` char(200) NOT NULL,
+  `loginID` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `eventID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `eventName` char(250) NOT NULL,
+  `eventDescription` text NOT NULL,
+  `eventCategory` char(200) NOT NULL,
+  `eventAddress` varchar(200) NOT NULL,
+  `eventLocation` char(200) NOT NULL,
+  `eventDate` char(15) NOT NULL,
+  `eventTime` char(10) NOT NULL,
+  `eventImageCover` blob NOT NULL,
+  PRIMARY KEY (`eventID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `loginID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `AcessRights` char(10) NOT NULL,
+  PRIMARY KEY (`loginID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `loginID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `FullName` char(200) NOT NULL,
+  `PhoneNumber` char(10) NOT NULL,
+  `DateOfBirth` char(15) NOT NULL,
+  KEY `loginID` (`loginID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `login`
+--
+ALTER TABLE `login`
+  ADD CONSTRAINT `login_id` FOREIGN KEY (`loginID`) REFERENCES `users` (`loginID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
