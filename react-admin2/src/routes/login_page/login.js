0f98a31ba1fc26ca7963cbb29f3a2940a8bb6afb
theavidcoder24 <malloriecini@outlook.com>
2021-05-10 08:28:53 +1000

    Update login.js

--- react-admin2/src/routes/login_page/login.js
+++ react-admin2/src/routes/login_page/login.js
@@ -13,45 +13,43 @@ import React, { useState } from 'react';
 //     }
 // });
 
-export function login(props) {
+export function Login(props) {
     // Declare a new state variable, which we'll call "count"  
     const [count, setCount] = useState(0);
-    const handleLogin = () => {
-        document.getElementById("loginmessage").innerHTML = "Loading...";
-        var log_email = document.getElementById("log_email");
-        var log_pass = document.getElementById("log_pass");
-        var login_details = new FormData();
-        login_details.set('action', 'login');
-        login_details.set('log_email', log_email.value);
-        login_details.set('log_pass', log_pass.value);
-        // login_details.set('login_user', login_user.value);
-        // each form element goes into the login_details object ^
-        fetch('http://localhost/eventee2/api/ws.php?action=adminLogin', {
-            method: 'POST',
-            body: login_details,
-            credentials: 'include',
+    document.getElementById("loginmessage").innerHTML = "Loading...";
+    var log_email = document.getElementById("log_email");
+    var log_pass = document.getElementById("log_pass");
+    var login_details = new FormData();
+    login_details.set('action', 'login');
+    login_details.set('log_email', log_email.value);
+    login_details.set('log_pass', log_pass.value);
+    // login_details.set('login_user', login_user.value);
+    // each form element goes into the login_details object ^
+    fetch('http://localhost/eventee2/api/ws.php?action=adminLogin', {
+        method: 'POST',
+        body: login_details,
+        credentials: 'include',
+    })
+        .then(function (response) {
+            // Force error into console
+            response.text().then(function (text) {
+                console.log(text);
+            });
+            // HTTP Response Codes
+            if (response.status === 202) {
+                localStorage.setItem('login', "true"); document.getElementById("loginmessage").innerHTML = "Success Your Logged In";
+                props.setCount("Logged In");
+                console.log('Login Successful');
+            }
+            if (response.status === 401) {
+                console.log('Not permitted');
+                document.getElementById("loginmessage").innerHTML = "Not implemented";
+            }
+            if (response.status === 501) {
+                console.log('Not implemented');
+                document.getElementById("loginmessage").innerHTML = "Try Again";
+            }
         })
-            .then(function (response) {
-                // Force error into console
-                response.text().then(function (text) {
-                    console.log(text);
-                });
-                // HTTP Response Codes
-                if (response.status === 202) {
-                    localStorage.setItem('login', "true"); document.getElementById("loginmessage").innerHTML = "Success Your Logged In";
-                    props.setCount("Logged In");
-                    console.log('Login Successful');
-                }
-                if (response.status === 401) {
-                    console.log('Not permitted');
-                    document.getElementById("loginmessage").innerHTML = "Not implemented";
-                }
-                if (response.status === 501) {
-                    console.log('Not implemented');
-                    document.getElementById("loginmessage").innerHTML = "Try Again";
-                }
-            })
-    }
 
     // const classes = useStyles();
 
