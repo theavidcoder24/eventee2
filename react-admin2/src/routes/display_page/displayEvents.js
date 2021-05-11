2b0544373adddefb68042c8269c7c240af991800
theavidcoder24 <malloriecini@outlook.com>
2021-05-11 14:02:57 +1000

    works render

--- react-admin2/src/routes/display_page/displayEvents.js
+++ react-admin2/src/routes/display_page/displayEvents.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import React, { useState } from 'react';
 // import { createUseStyles } from 'react-jss';
 // import CreateEvents from './routes/create_page/createEvents.js';
@@ -7,82 +7,33 @@ import React from 'react';
 
 /* - Display Events - */
 function DisplayEvents() {
+  const [event, setEvent] = useState([]);
+
+  useEffect(() => {
+    pullEvents();
+  }, []);
+
   function pullEvents() {
-    // Define output string that will display the database data
-    var outStr = '';
-    var outStr2 = '';
     fetch('http://localhost/eventee2/api/ws.php?action=displayEvents', {
       method: "GET",
       credentials: "include",
-    })
-      .then(function (response) {
-        response.json().then(function (data) {
-          localStorage.setItem("events", JSON.stringify(data));
-          // console.log(data);
-          data.forEach(row => {
-            outStr +=
-              '<tr><td>' + row.eventID +
-              '</td><td>' + row.eventName +
-              '</td><td>' + row.eventDescription +
-              '</td><td>' + row.eventCategory +
-              '</td><td>' + row.eventAddress +
-              '</td><td>' + row.eventLocation +
-              '</td><td>' + row.eventDate +
-              '</td><td>' + row.eventTime +
-              '</td><td><button href="#update_events" class="modal-trigger" onclick="fillUpdate(' + row.eventID + ')" value="' + row.eventID + '"><i class="material-icons">edit</i></button>' +
-              '</td><td>' + '<button onclick="DeleteRemoveEvent(' + row.eventID + ')" value="' + row.eventID + '"><i class="material-icons">delete</i></button>' +
-              '</td></tr>';
-            outStr2 =
-              `<div class="modal-content">
-              <h5 class="modal-close right" onclick="closeModal()">&#10005;</h5>
-                <h4>Update Events</h4>
-                  <form action="api/ws.php" method="POST" onclick="return preventDefault()" novalidate>
-                      <div class="input-field">
-                          <i class="material-icons prefix">title</i>
-                          <input id="update_ev_name" name="update_ev_name" placeholder="Event Name" type="text">
-                      </div>
-                      <div class="input-field">
-                          <i class="material-icons prefix">notes</i>
-                          <textarea id="update_ev_desc" name="update_ev_desc" class="materialize-textarea"
-                              placeholder="Event Description"></textarea>
-                      </div>
-                      <div class="input-field">
-                          <i class="material-icons prefix">category</i>
-                          <input type="text" id="update_ev_cat" name="update_ev_cat" placeholder="Enter Your Event Type">
-                      </div>
-                      <div class="input-field col s6">
-                          <i class="material-icons prefix">map</i>
-                          <input type="text" id="update_ev_address" name="update_ev_address" placeholder="Enter Your Address">
-                      </div>
-                      <div class="input-field col s6">
-                          <i class="material-icons prefix">flag</i>
-                          <input type="text" id="update_ev_loc" name="update_ev_loc" placeholder="Event Location">
-                      </div>
-                      <div class="input-field col s6">
-                          <i class="material-icons prefix">date_range</i>
-                          <input type="text" id="update_ev_date" class="datepicker" placeholder="Event Date">
-                      </div>
-                      <div class="input-field col s6">
-                          <i class="material-icons prefix">schedule</i>
-                          <input type="text" id="update_ev_time" class="timepicker" placeholder="Event Time">
-                      </div>
-                      <input type="hidden" name="eventid" value="`+ row.eventID + `" id="eventid">
-                      <input type="hidden" name="action" value="update" id="updateEvent">
-                      <button class="btn waves-effect waves-light" type="submit" onclick="PostUpdateEvent()">Update
-                          Event</button>
-                  </form>
-              </div>`
-          });
-          document.getElementById('eventsTable').innerHTML = outStr;
-          document.getElementById('update_events').innerHTML = outStr2;
+    }).then((res) => {
+      if (res.status === 202) {
+        res.json().then((data) => {
+          setEvent(data);
+          console.log(data);
         });
-      });
+      }
+      if (res.status === 501) {
+        setEvent("no event");
+      }
+    })
   }
 
   return (
     <div>
       <h2>Events List</h2>
-      <button onClick={pullEvents()}>Display Events</button>
+      {/* <button onClick={pullEvents()}>Display Events</button> */}
       <div className="content">
         <table>
           <th>Event ID</th>
@@ -93,9 +44,22 @@ function DisplayEvents() {
           <th>Event Location</th>
           <th>Event Date</th>
           <th>Event Time</th>
-          <table id="eventsTable" className="striped">
-          </table>
+          {/* <table id="eventsTable" className="striped">
+          </table> */}
         </table>
+        {event.map((event, index) => (
+          <tr>
+            <td>{event.eventID}</td>,
+            <td>{event.eventName}</td>,
+            <td>{event.eventDescription}</td>,
+            <td>{event.eventCategory}</td>,
+            <td>{event.eventAddress}</td>,
+            <td>{event.eventLocation}</td>,
+            <td>{event.eventDate}</td>,
+            <td>{event.eventTime}</td>
+          </tr>
+        ))}
+
       </div>
     </div>
   )
