1bd597379570f1a0ef8854bde6f7f8a20bbf6f8a
theavidcoder24 <malloriecini@outlook.com>
2021-06-01 12:19:28 +1000

    fixed sw

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,7 +1,7 @@
 // This code is used to register a service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('js/sw.js').then(function (registration) {
+    navigator.serviceWorker.register('src/js/sw.js').then(function (registration) {
       // Registration success!
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function (err) {
@@ -34,12 +34,31 @@ self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
       .then(function (response) {
-        // Cache reached so return a response
+        // Cache hit - so return response
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-      )
+
+        return fetch(event.request).then(
+          function (response) {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function (cache) {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          }
+        );
+      })
   );
 });
