8440cd67160d781e30fcee277911b085f28f3d6c
Mallorie <malloriecini@outlook.com>
2021-06-04 23:32:41 +1000

    Update ws.php

--- api/ws.php
+++ api/ws.php
@@ -23,7 +23,8 @@ if (!isset($_SERVER['HTTP_REFERER'])) {
 require('session.php');
 
 // Checks that the referer matches the defined if it's not then end
-if ($_SERVER['HTTP_REFERER'] !== "http://localhost/eventee2/" || $_SERVER['HTTP_REFERER'] == "http://localhost:3000/") {
+if ($_SERVER['HTTP_REFERER'] == "http://localhost/eventee2/" || $_SERVER['HTTP_REFERER'] == "http://localhost:3000/") {
+    // For debugging purposes and checking address use !== instead 
     // 192.168.43.225
     // https://malloriecini.com/
     echo "Valid address";
