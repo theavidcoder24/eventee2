d9742e333a1c0a6ea95721bb73c4483799c9aaca
Mallorie <malloriecini@outlook.com>
2021-06-05 00:06:28 +1000

    updates for proj4

---
+++ api/sql/emptyeventee2.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 04, 2021 at 01:53 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eventee2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendees`
+--
+
+DROP TABLE IF EXISTS `attendees`;
+CREATE TABLE IF NOT EXISTS `attendees` (
+  `userID` int(10) UNSIGNED NOT NULL,
+  `eventID` int(11) UNSIGNED NOT NULL,
+  `FullName` char(200) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `eventName` char(250) NOT NULL,
+  `eventAttendance` char(10) NOT NULL DEFAULT 'Not Going',
+  KEY `userID` (`userID`),
+  KEY `eventID` (`eventID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `changelog`
+--
+
+DROP TABLE IF EXISTS `changelog`;
+CREATE TABLE IF NOT EXISTS `changelog` (
+  `changelogID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date` timestamp(6) NOT NULL DEFAULT current_timestamp(6),
+  `browser` char(200) NOT NULL,
+  `ip` char(15) NOT NULL,
+  `action_type` char(200) NOT NULL,
+  `UserID` int(10) UNSIGNED DEFAULT NULL,
+  PRIMARY KEY (`changelogID`),
+  KEY `UserID` (`UserID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `eventID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `eventName` char(250) NOT NULL,
+  `eventDescription` text NOT NULL,
+  `eventCategory` char(200) NOT NULL,
+  `eventAddress` varchar(200) NOT NULL,
+  `eventLocation` char(200) NOT NULL,
+  `eventDate` char(15) NOT NULL,
+  `eventTime` char(10) NOT NULL,
+  PRIMARY KEY (`eventID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users2`
+--
+
+DROP TABLE IF EXISTS `users2`;
+CREATE TABLE IF NOT EXISTS `users2` (
+  `UserID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `FullName` char(200) NOT NULL,
+  `PhoneNumber` char(10) NOT NULL,
+  `DateOfBirth` char(15) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `UserPassword` varchar(255) NOT NULL,
+  `AccessRights` char(200) NOT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `changelog`
+--
+ALTER TABLE `changelog`
+  ADD CONSTRAINT `fk_logUser` FOREIGN KEY (`UserID`) REFERENCES `users2` (`UserID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
