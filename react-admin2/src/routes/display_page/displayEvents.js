9a656979142b4b2f2846c49027b3edebcc08f983
theavidcoder24 <malloriecini@outlook.com>
2021-06-09 16:52:45 +1000

    Update displayEvents.js

--- react-admin2/src/routes/display_page/displayEvents.js
+++ react-admin2/src/routes/display_page/displayEvents.js
@@ -36,26 +36,13 @@ function DisplayEvents() {
   function DeleteRemoveEvent(eventid) {
     console.log("Delete event with id " + eventid);
 
-    // events = JSON.parse(localStorage.getItem("events"));
-
-    // selectedEvent = events.filter(event => event[0] == eventid)[0];
-
-    // if (selectedEvent != null) {
-    //   console.log(selectedEvent);
-    //   document.getElementById("update_ev_name").value = selectedEvent[1];
-    //   document.getElementById("update_ev_desc").value = selectedEvent[2];
-    //   document.getElementById("update_ev_cat").value = selectedEvent[3];
-    //   document.getElementById("update_ev_address").value = selectedEvent[4];
-    //   document.getElementById("update_ev_loc").value = selectedEvent[5];
-    //   document.getElementById("update_ev_date").value = selectedEvent[6];
-    //   document.getElementById("update_ev_time").value = selectedEvent[7];
-    // }
-
     var removefd = new FormData();
     removefd.append('action', 'DeleteRemoveEvent');
     removefd.append('eventid', eventid);
 
     fetch('http://localhost/eventee2/api/ws.php?action=removeEvent', {
+      // http://localhost/eventee2/api/ws.php?action=removeEvent
+      // https://adminpanel.malloriecini.com/api/ws.php?action=removeEvent
       method: 'POST',
       body: removefd,
       credentials: 'include'
