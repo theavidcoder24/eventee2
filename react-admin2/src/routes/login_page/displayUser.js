8aaa9fd830e69b7f3df21b0e9ee14b1358808b2e
Mallorie <malloriecini@outlook.com>
2021-06-13 13:08:49 +1000

    populate input fields in update user

--- react-admin2/src/routes/login_page/displayUser.js
+++ react-admin2/src/routes/login_page/displayUser.js
@@ -86,83 +86,85 @@ function DisplayUser() {
         <div>
             <h3>User Profile</h3>
             <div className="content">
-                <table className="striped">
-                    <th>User ID</th>
-                    <th>Full Name</th>
-                    <th>Phone Number</th>
-                    <th>Email</th>
-                    <th>Date Of Birth</th>
-                    <th>AccessRights</th>
-                    {user.map((user, index) => (
-                        <tr>
-                            <td>{user.UserID}</td>
-                            <td>{user.FullName}</td>
-                            <td>{user.PhoneNumber}</td>
-                            <td>{user.Email}</td>
-                            <td>{user.DateOfBirth}</td>
-                            <td>{user.AccessRights}</td>
-                        </tr>
-                    ))}
-                </table>
-            </div>
-            <br></br>
-            <div>
-                <p id="successmessage" className="green"></p>
-                <p id="errormessage" className="red"></p>
-                <h4>Edit User Profile</h4>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="row">
-                        <div className="input-field col s12">
-                            <i className="material-icons prefix">badge</i>
-                            <select className="icons" id="update_access_rights">
-                                <option value="" disabled selected>Access Rights</option>
-                                <option value="Admin">Admin</option>
-                                <option value="User">User</option>
-                            </select>
-                            <span className="helper-text" data-error="Please enter a valid category"
-                                data-success="Correct" max="20"></span>
+                {user.map((user, index) => (
+                    <div>
+                        <p id="successmessage" className="green"></p>
+                        <p id="errormessage" className="red"></p>
+                        <div>
+                            <table className="striped">
+                                <th>User ID</th>
+                                <th>Full Name</th>
+                                <th>Phone Number</th>
+                                <th>Email</th>
+                                <th>Date Of Birth</th>
+                                <th>AccessRights</th>
+                                <tr>
+                                    <td>{user.UserID}</td>
+                                    <td>{user.FullName}</td>
+                                    <td>{user.PhoneNumber}</td>
+                                    <td>{user.Email}</td>
+                                    <td>{user.DateOfBirth}</td>
+                                    <td>{user.AccessRights}</td>
+                                </tr>
+                            </table>
                         </div>
-                        <div className="input-field col s12">
-                            <i className="material-icons prefix">account_circle</i>
-                            <input id="update_user_name" type="text" className="validate tooltipped" placeholder="Full Name"
-                                data-position="top" data-tooltip="Must be more than 2 characters" {...register("updateusername", { required: true, minLength: 2 })}></input>
-                            {/* errors will return when field validation fails  */}
-                            {errors.updateusername && <span>This field is required</span>}
-                            <span className="helper-text" data-error="Please enter a valid name"
-                                data-success="Correct"></span>
-                            <div className="errorMsg"></div>
-                        </div>
-                        <div className="input-field col s12">
-                            <i className="material-icons prefix">phone</i>
-                            <input id="update_user_phone" type="tel" className="validate" placeholder="Phone Number" {...register("update_user_phone", { required: true, minLength: 10 })}></input>
-                            {/* errors will return when field validation fails  */}
-                            {errors.updateuserphone && <span>This field is required</span>}
-                            <span className="helper-text" data-error="Please enter a valid phone number"
-                                data-success="Correct"></span>
-                        </div>
-                        <div className="input-field col s12">
-                            <i className="material-icons prefix">email</i>
-                            <input id="update_user_email" name="reg_email" type="email" className="validate"
-                                placeholder="Email" {...register("updateuseremail", { required: true, minLength: 2 })}></input>
-                            {/* errors will return when field validation fails  */}
-                            {errors.updateuseremail && <span>This field is required</span>}
-                            <span className="helper-text" data-error="Please enter a valid email"
-                                data-success="Correct"></span>
-                        </div>
-                        <div className="input-field col s12">
-                            <i className="material-icons prefix">cake</i>
-                            <input type="text" id="update_user_dob" className="datepicker validate"
-                                placeholder="Date of Birth *" {...register("updateuserdob", { required: true, minLength: 2 })}></input>
-                            {/* errors will return when field validation fails  */}
-                            {errors.updateuserdob && <span>This field is required</span>}
-                            <span className="helper-text" data-error="Please enter a valid date of birth"
-                                data-success="Correct" max="20"></span>
-                        </div>
+                        <br></br>
+                        <h4>Edit User Profile</h4>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">badge</i>
+                                    <select className="icons" id="update_access_rights">
+                                        <option value="" disabled selected>Access Rights</option>
+                                        <option defaultValue="Admin">Admin</option>
+                                        <option defaultValue="User">User</option>
+                                    </select>
+                                    <span className="helper-text" data-error="Please enter a valid category"
+                                        data-success="Correct" max="20"></span>
+                                </div>
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">account_circle</i>
+                                    <input id="update_user_name" type="text" className="validate tooltipped" defaultValue={user.FullName} placeholder="Full Name"
+                                        data-position="top" data-tooltip="Must be more than 2 characters" {...register("updateusername", { required: true, minLength: 2 })}></input>
+                                    {/* errors will return when field validation fails  */}
+                                    {errors.updateusername && <span>This field is required</span>}
+                                    <span className="helper-text" data-error="Please enter a valid name"
+                                        data-success="Correct"></span>
+                                    <div className="errorMsg"></div>
+                                </div>
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">phone</i>
+                                    <input id="update_user_phone" type="tel" className="validate" defaultValue={user.PhoneNumber} placeholder="Phone Number" {...register("update_user_phone", { required: true, minLength: 10 })}></input>
+                                    {/* errors will return when field validation fails  */}
+                                    {errors.updateuserphone && <span>This field is required</span>}
+                                    <span className="helper-text" data-error="Please enter a valid phone number"
+                                        data-success="Correct"></span>
+                                </div>
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">email</i>
+                                    <input id="update_user_email" name="reg_email" type="email" className="validate" defaultValue={user.Email}
+                                        placeholder="Email" {...register("updateuseremail", { required: true, minLength: 2 })}></input>
+                                    {/* errors will return when field validation fails  */}
+                                    {errors.updateuseremail && <span>This field is required</span>}
+                                    <span className="helper-text" data-error="Please enter a valid email"
+                                        data-success="Correct"></span>
+                                </div>
+                                <div className="input-field col s12">
+                                    <i className="material-icons prefix">cake</i>
+                                    <input type="text" id="update_user_dob" className="datepicker validate" defaultValue={user.DateOfBirth}
+                                        placeholder="Date of Birth *" {...register("updateuserdob", { required: true, minLength: 2 })}></input>
+                                    {/* errors will return when field validation fails  */}
+                                    {errors.updateuserdob && <span>This field is required</span>}
+                                    <span className="helper-text" data-error="Please enter a valid date of birth"
+                                        data-success="Correct" max="20"></span>
+                                </div>
+                            </div>
+                            <input type="hidden" name="userid" value="`+ row.UserID + `" id="userid"></input>
+                            <input type="hidden" name="action" value="admin_update" id="admin_update"></input>
+                            <button id="admin_update" className="btn indigo waves-effect waves-light" value="' + row.UserID + '" onClick={editUser} type="submit" name="admin_update">Update User</button>
+                        </form>
                     </div>
-                    <input type="hidden" name="userid" value="`+ row.UserID + `" id="userid"></input>
-                    <input type="hidden" name="action" value="admin_update" id="admin_update"></input>
-                    <button id="admin_update" className="btn indigo waves-effect waves-light" value="' + row.UserID + '" onClick={editUser} type="submit" name="admin_update">Update User</button>
-                </form>
+                ))}
             </div>
         </div>
     )
